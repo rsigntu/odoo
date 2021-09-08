407e0aedf76a2dd1be9777747912017e3296df64
Ravi Singh <rsigntu@gmail.com>
2021-09-08 17:15:11 +0530

    Employee Birthday Wishes

---
+++ employee_birthday_wishes/__manifest__.py
@@ -0,0 +1,11 @@
+{
+	"name": "HR Employee Birthday Wishes",
+	"description": "Send Birthday Wishes to Employee on their Birthday automatically",
+	"depends": ["mail","hr"],
+	"data": [
+		"security/birthday_schduler.xml",
+		"views/email_template_view.xml"
+	],
+	"author": "Ravi Singh",
+	"installable": True,
+}
