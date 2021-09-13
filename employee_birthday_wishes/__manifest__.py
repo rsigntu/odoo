91de55f75efffdf5dffd2fbf374da0811a3dfbf2
Ravi Singh <rsigntu@gmail.com>
2021-09-13 12:19:32 +0530

    misc

--- employee_birthday_wishes/__manifest__.py
+++ employee_birthday_wishes/__manifest__.py
@@ -1,6 +1,6 @@
 {
 	"name": "HR Employee Birthday Wishes",
-	"description": "Send Birthday Wishes to Employee on their Birthday automatically",
+	"description": "Odoo Website Birthday Reminder module allows to send birthday greetings to customers on their birthday.A cron scheduler is running which help to send birthday greetings automatically to the customer.",
 	"depends": ["mail","hr"],
 	"author": "Ravi Singh Parihar",
 	"version": "14.0.0.1",
@@ -14,6 +14,5 @@
 	]
 	"price": 5,
 	"currency": "USD",
-	"author": "Ravi Singh",
 	"installable": True,
 }
