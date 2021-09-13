7b4a9cbcb8d9689311e4797327c60a35ea6e091f
Ravi Singh <rsigntu@gmail.com>
2021-09-13 10:01:10 +0530

    Price Updated

--- employee_birthday_wishes/__manifest__.py
+++ employee_birthday_wishes/__manifest__.py
@@ -2,10 +2,13 @@
 	"name": "HR Employee Birthday Wishes",
 	"description": "Send Birthday Wishes to Employee on their Birthday automatically",
 	"depends": ["mail","hr"],
+	"author": "Ravi Singh Parihar",
 	"data": [
 		"security/birthday_schduler.xml",
 		"views/email_template_view.xml"
 	],
+	"price": 5,
+	"currency": "USD",
 	"author": "Ravi Singh",
 	"installable": True,
 }
