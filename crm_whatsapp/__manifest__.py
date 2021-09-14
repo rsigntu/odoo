0f16a6e79aabc8c831d001b0d51af21511f700c9
Ravi Singh <rsigntu@gmail.com>
2021-09-14 17:16:41 +0530

    [ADD] crm_whatsapp module

---
+++ crm_whatsapp/__manifest__.py
@@ -0,0 +1,19 @@
+{
+	"name": "Whatsapp Integration in CRM",
+	"description": "Add whatsapp Button in crm related to the Contact Person",
+	"depends": ["crm"],
+	"category": "crm",
+	"author": "Ravi Singh Parihar",
+	"images": [
+		"static/description/icon.jpeg"
+	],
+	"price": "10",
+	"currency": "USD",
+	"version": "14.0.0.1",
+	"data": [
+		"security/ir.model.access.csv",
+		"views/crm_lead_views.xml"
+	],
+
+	"installable": True,
+}
